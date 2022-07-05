6792b7a3765ff9570f4dcf46759353139faa4e85
Shahar Tal <chapo12345@gmail.com>
2022-07-05 23:33:20 +0300

    fixed tests describe desctiption

--- src/use-cases/measure-category/list-measures-categories.test.js
+++ src/use-cases/measure-category/list-measures-categories.test.js
@@ -11,7 +11,7 @@ const dbPath = process.env.SQLITE_DB_PATH
 
 const dbConnector = makeDbConnector({ dbPath })
 
-describe('getMeasure', () => {
+describe('listMeasuresCategories', () => {
   const measuresCategoriesDb = makeMeasuresCategoriesDb({ dbConnector })
 
   const listMeasuresCategories = makeListMeasuresCategories({ measuresCategoriesDb })
--- src/use-cases/measure/list-measures.test.js
+++ src/use-cases/measure/list-measures.test.js
@@ -15,7 +15,7 @@ const dbPath = process.env.SQLITE_DB_PATH
 
 const dbConnector = makeDbConnector({ dbPath })
 
-describe('getMeasure', () => {
+describe('listMeasures', () => {
   const measuresDb = makeMeasuresDb({ dbConnector })
   const measuresCategoriesDb = makeMeasuresCategoriesDb({ dbConnector })
   const measuresValuesTypesDb = makeMeasuresValuesTypesDb({ dbConnector })
