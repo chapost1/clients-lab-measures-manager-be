f228b7fdf1f96d1d6770687a5252450adc029ef1
Shahar Tal <chapo12345@gmail.com>
2022-07-08 15:40:55 +0300

    added some hint to see why it fails on ci only

--- src/data-access/sqlite/error-handler/error-handler.js
+++ src/data-access/sqlite/error-handler/error-handler.js
@@ -77,7 +77,7 @@ module.exports = function makeErrorHandler ({ DbConflictError, DbInvalidError, D
     }
 
     if (!(error instanceof SqliteError)) {
-      console.log(error)
+      console.log(`Error type: ${error.name} Error: ${error}`)
       return new DbApplicationError('invalid error (not SqliteError)')
     }
 
