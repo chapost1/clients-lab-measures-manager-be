5fcccaca1d94f1bd832b0953d6032db5d9d08902
Shahar Tal <chapo12345@gmail.com>
2022-07-10 11:16:20 +0300

    added clients routes base structure

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,13 @@
 const routes = require('express').Router()
 
+const clientsRoutes = require('./clients/index')
 const measuresRoutes = require('./measures/index')
 const measuresCategoriesRoutes = require('./measures-categories/index')
 
 routes.use('/measures', measuresRoutes)
 
 routes.use('/measures-categories', measuresCategoriesRoutes)
+
+routes.use('clients', clientsRoutes)
 
 module.exports = routes
